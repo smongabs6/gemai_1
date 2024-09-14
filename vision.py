ef361de0c4f6cbc9dcc269c6a9cc5a059609f39b
smongabs6 <smongabs6@gmail.com>
2024-09-14 07:58:08 +0530

    14sepcommit_1

--- vision.py
+++ vision.py
@@ -16,26 +16,32 @@ def get_gemini_response(input,image):
     return(response.text)
 
 st.set_page_config(page_title="Tell me About Image Demo")
-st.header("My First Gemini Image LLM Based Application")
-st.write("Developed by Sandeep Monga")
 
-input=st.text_input("Input: ",key="input")
+st.header("Generative AI Application using Gemini Pro LLM Model.. ")
+st.write('''Developed an application using the Gemini Pro model, a powerful Large Language Model (LLM), to demonstrate its capabilities. The app allows users to upload an image and performs three functions: identifying objects within the image, generating a summary of its content, and answering user questions related to the image. This showcases the integration of visual recognition and natural language processing in AI solutions.
+''')
+
+
 image=""
 #cam_click=st.button("Click to Open Camera...")
 #if cam_click:
-cam_input=st.camera_input("camera input",key="cam_input")
+uploaded_file=st.file_uploader("Choose an image...",type=["jpg","jpeg","png"])
+if uploaded_file:
+    image=Image.open(uploaded_file)
+    st.image(image,caption="Uploaded image",use_column_width=True)
+
+st.write("Or")
+cam_input=st.camera_input("Upload the image using camera...",key="cam_input")
 if cam_input:
     image=Image.open(cam_input)
     st.image(image,caption="Uploaded image",use_column_width=True)
-else:
-    uploaded_file=st.file_uploader("Choose an image...",type=["jpg","jpeg","png"])
-    if uploaded_file:
-        image=Image.open(uploaded_file)
-        st.image(image,caption="Uploaded image",use_column_width=True)
+   
 
-submit=st.button("Tell me something about this image..")
+input=st.text_input("Ask the Question related to Uploaded image (its Optional)",key="input")
+
+submit=st.button("Provide insights about this image..")
 if submit:
     response=get_gemini_response(input,image)
     st.write(response)
-else:
-    st.write("No image selected")
+st.write("--------------------------------------------------------------------------------------------")
+st.write("Developed by Sandeep Monga")
